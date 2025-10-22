4ac14da933a668eb46eeaa45c8f9210a36a4b7cd
ssshinnpson <ddae0000@gmail.com>
2025-10-22 11:04:57 +0900

    [feat]

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "52.78.176.34"]
 
 
 # Application definition
